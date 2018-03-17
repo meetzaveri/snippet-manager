cb508c86b2ae00fda2d130b2a37fbddf3bf65b31
Meet Zaveri <meethpatel96@gmail.com>
2018-03-17 16:49:27 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url : 'mongodb://meetzaveri:mlab7771@ds261828.mlab.com:61828/code-snippet-manager'
+    url : 'mongodb://<username>:<password>@ds261828.mlab.com:61828/code-snippet-manager'
   };
